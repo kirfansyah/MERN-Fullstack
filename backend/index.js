978737ffedd0e70a5a05e017ca0353c9c06eeeda
unknown <kikiirfansyah33@gmail.com>
2023-03-15 14:46:43 +0700

    backend done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+import UserRoute from "./routes/UserRoute.js"
 
 const app = express()
+mongoose.connect('mongodb://0.0.0.0:27017/fullstack_db', { // mongodb://0.0.0.0:27017/fullstack_db <-- pake ini gak konek databasenya
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Database Connected...'))
+
+app.use(cors())
+app.use(express.json())
+app.use(UserRoute)
 
 app.listen(5000, () => console.log('Server up and running ...'))
+
